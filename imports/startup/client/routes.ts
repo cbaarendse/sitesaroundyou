2d972cffb7276d49dcc0be8301061154aedc1961
Constantijn Baarendse <cbaarendse@live.nl>
2018-04-24 16:45:54 +0200

    update #4

--- imports/startup/client/routes.ts
+++ imports/startup/client/routes.ts
@@ -6,15 +6,3 @@ import '../../ui/pages/home/home.ts';
 import '../../ui/pages/not-found/not-found.ts';
 
 // Set up all routes in the app
-FlowRouter.route('/', {
-  name: 'App.home',
-  action() {
-    BlazeLayout.render('App_body', { main: 'App_home' });
-  },
-});
-
-FlowRouter.notFound = {
-  action() {
-    BlazeLayout.render('App_body', { main: 'App_notFound' });
-  },
-};
