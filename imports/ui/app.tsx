170cd82181393d36dda640809a06eba54bf495fa
Constantijn Baarendse <cbaarendse@live.nl>
2018-04-30 19:20:22 +0200

    update #15

--- imports/ui/app.tsx
+++ imports/ui/app.tsx
@@ -7,7 +7,7 @@ import ReactDOM from 'react-dom';
 export class App extends React.Component {
     render() {
         return (
-            <h1>Hallo Wereld!</h1>;
+            <h1>Hallo Wereld!</h1>
         )
     }
 }
