409da9146c5f8eaf3bf1083627f301532a963e91
Constantijn Baarendse <cbaarendse@live.nl>
2018-05-18 18:10:45 +0200

    update #22

--- imports/ui/map.tsx
+++ imports/ui/map.tsx
@@ -1,4 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-export const Map = () => <div className='content'>Map</div>;
+const googleMapsClient = require('@google/maps').createClient({
+    key: Meteor.settings.public.googleMapsAPIKey,
+    Promise: 'Promise'
+});
+
+export const Map = () => <div className='content'>
+    <div id="map"></div>
+</div>;
+
