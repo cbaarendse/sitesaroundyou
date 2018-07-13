a0f0cc420801fd372b0898ab2258917ca67a809e
Constantijn Baarendse <cbaarendse@live.nl>
2018-07-13 15:32:34 +0200

    update #42

--- imports/ui/map.tsx
+++ imports/ui/map.tsx
@@ -1,23 +1,31 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
-import { Map, InfoWindow, Market, GoogleApiWrapper } from 'google-maps-react';
+import { Map, InfoWindow, Market, GoogleApiWrapper } from 'google-map-react';
 
 //TODO:
 export class MapContainer extends React.Component {
-    render() {
-        return (
-            <Map google={this.props.google} zoom={14}>
-                <Marker onClick={this.onMarkerClick}
-                    name={'Current location'} />
-                <InfoWindow onClose={this.onInfoWindowClose}>
-                    <div>
-                        <h1>{this.state.selectedPlace.name}</h1>
-                    </div>
-                </InfoWindow>
-            </Map>
-        );
-    }
+    onMarkerClick: function(props, marker, e) {
+    this.setState({
+        selectedPlace: props,
+        activeMarker: marker,
+        showingInfoWindow: true
+    });
+},
+
+render() {
+    return (
+        <Map google={this.props.google} zoom={14}>
+            <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+            <InfoWindow onClose={this.onInfoWindowClose}>
+                <div>
+                    <h1>{this.state.selectedPlace.name}</h1>
+                </div>
+            </InfoWindow>
+        </Map>
+    );
+}
 }
 
 export default GoogleApiWrapper({
